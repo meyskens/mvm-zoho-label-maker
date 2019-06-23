0fc61fe4b91a0bf3da758109b51ce82d3631164c
Maartje Eyskens <maartje@eyskens.me>
2019-06-23 21:17:08 +0200

    Initial commit with 2 labels
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ README.md
@@ -0,0 +1,16 @@
+MvM Zoho Label Maker
+========
+
+This repo contains a Zoho widget that as designed to be used as a related list widget. It contains a PDF maker to print labels from the data in Zoho CRM.
+
+## Setup
+
+1) Install the Widget SDK `npm install -g zoho-extension-toolkit`
+2) Package it using `zet pack`
+3) Upload it as a hosted widget in Zoho
+
+## Background
+This is developed for [Moeders voor Moeders](http://www.moedersvoormoeders.be/) which uses the Zoho CRM suite to keep member data.
+
+## Notes
+This documentation is in English while the original application is in Dutch.
---
+++ labelmaker/app/js/app.js
@@ -0,0 +1,59 @@
+// Global variables, contain test info if ran outside of ZOHO
+var mvmNumber = "MVM0000";
+var name = "Maartje Eyskens";
+var code = ""
+var numVolw = 0;
+var numKind = 0;
+
+function initializeWidget()
+{
+	/*
+	 * Subscribe to the EmbeddedApp onPageLoad event before initializing the widget 
+	 */
+	ZOHO.embeddedApp.on("PageLoad",function(data) {
+		$('#nodata-alert').alert('close')
+		if(!data || !data.Entity) {
+			$('#nodata-alert').alert()
+			return
+		}
+		ZOHO.CRM.API.getRecord({Entity:data.Entity,RecordID:data.EntityId})
+		.then(function(response) {
+			console.log(response.data[0])
+
+			name = `${response.data[0].Voornaam} ${response.data[0].Account_Name}`;
+			mvmNumber = response.data[0].Doelgroep_nummer;
+			code = response.data[0].Code;
+			numKind = response.data[0].Aantal_12; // Aantal < 12
+			numVolw = response.data[0].Aantal_121; // Aantal > 12
+		});		
+	})
+	/*
+	 * initialize the widget.
+	 */
+	ZOHO.embeddedApp.init();
+}
+
+function makeKaartjeVoeding() {
+	const doc = new jsPDF({
+		orientation: 'landscape',
+		unit: 'mm',
+		format: [ 85.60,  53.98 ] // ISO Credit Card
+	})
+	doc.text(mvmNumber, 10, 10)
+	doc.text(name, 10, 20)
+	doc.text(`${numVolw}V + ${numKind}K`, 10, 30)
+	doc.text(code, 10, 40)
+	doc.save(`${mvmNumber}-voeding.pdf`)
+}
+
+function makeKaartjeMateriaal() {
+	const doc = new jsPDF({
+		orientation: 'landscape',
+		unit: 'mm',
+		format: [ 85.60,  53.98 ] // ISO Credit Card
+	})
+	doc.text(mvmNumber, 10, 10)
+	doc.text(name, 10, 20)
+	doc.text(code, 10, 30)
+	doc.save(`${mvmNumber}-materiaal.pdf`)
+}
